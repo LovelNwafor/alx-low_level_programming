cf1b2adc899195cb7b64e7bba22afcf5930bc6e4
LovelNwafor <loveloluchukwu715@gmail.com>
2023-10-07 10:42:49 -0700

    p

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -27,6 +27,24 @@ else
 printf("Last digit of %d is %d and is 0\n",
 n, n % 10);
 }
-return(0);
+return (0);
 }
+#include <stdio.h>
+/**
+* main - Prints the alphabet.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
+
 
