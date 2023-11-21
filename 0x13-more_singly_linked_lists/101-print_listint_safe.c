9c8a5f76eddaaf0476b8b8ff8eeb38fe735d976f
LovelNwafor <loveloluchukwu715@gmail.com>
2023-11-21 03:44:19 -0800

    h

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,30 +1,33 @@
-#include "lists.h"
+i#include "lists.h"
 
 /**
- * print_listint_safe - prints list, avoids loop
- * @head: points to start of list
- * Return: number of nodes in size_t or exit 98
+ * print_listint_safe - Prints a list, avoiding loops.
+ * @head: Points to the start of the list.
+ * Return: Number of nodes in size_t or exits with code 98.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	/* declarations */
-	size_t nodeCount = 0;
-	/* check for null pointer */
-	while (head)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		nodeCount += 1;
+    /* Declarations */
+    size_t nodeCount = 0;
 
-		if (head > head->next)
-		{
-			head = head->next;
-		}
-		else
-		{
-			head = head->next;
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			break;
-		}
-	}
-	return (nodeCount);
+    /* Check for null pointer */
+    while (head)
+    {
+        printf("[%p] %d\n", (void *)head, head->n);
+        nodeCount += 1;
+
+        if (head > head->next)
+        {
+            head = head->next;
+        }
+        else
+        {
+            head = head->next;
+            printf("-> [%p] %d\n", (void *)head, head->n);
+            break;
+        }
+    }
+
+    return (nodeCount);
 }
+
